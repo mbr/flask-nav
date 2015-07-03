70d83cb87dd0ae13ccde43a18c3d5145d09f7d88
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-03 12:38:55 +0200

    Per default render nothing or a comment, if in debug mode.

--- flask_nav/renderers.py
+++ flask_nav/renderers.py
@@ -1,5 +1,12 @@
+from flask import current_app
+
 from visitor import Visitor
 
 
 class SimpleRenderer(Visitor):
-    pass  # TODO: Implement
+    def visit_object(self, node):
+        if current_app.debug:
+            return '<!-- no implementation in {} to render {} -->'.format(
+                self.__class__.__name__, node.__class__.__name__,
+            )
+        return ''
