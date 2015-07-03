92ae8d70e22494d8d1a843aab23f2892c71a2a5d
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-03 13:25:33 +0200

    Use Base and Simple Renderer.

--- flask_nav/renderers.py
+++ flask_nav/renderers.py
@@ -3,10 +3,14 @@ from flask import current_app
 from visitor import Visitor
 
 
-class SimpleRenderer(Visitor):
+class BaseRenderer(Visitor):
     def visit_object(self, node):
         if current_app.debug:
             return '<!-- no implementation in {} to render {} -->'.format(
                 self.__class__.__name__, node.__class__.__name__,
             )
         return ''
+
+
+class SimpleRenderer(Visitor):
+    pass
