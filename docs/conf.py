529ca12be27ec557344818ab28cf95bc5f8e6290
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-14 19:09:26 +0200

    Start developing version 0.5.dev1 (after release of 0.4)

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,8 @@ exclude_patterns = ['_build']
 project = u'Flask-Nav'
 copyright = u'2015, Marc Brinkmann'
 author = u'Marc Brinkmann'
-version = '0.4'
-release = '0.4.dev1'
+version = '0.5'
+release = '0.5.dev1'
 
 pygments_style = 'sphinx'
 html_theme = 'alabaster'
